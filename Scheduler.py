fc906d3c6c07add29f15a7dc5f8e51f49a9286b6
James Bury <39829343+jamesbury77@users.noreply.github.com>
2018-12-18 22:37:13 -0500

    scheduler now gets all the necessary data from the csv and prints it out

--- Scheduler.py
+++ Scheduler.py
@@ -3,7 +3,9 @@ import csv
 row_number = 0
 column_number = 0
 
-LAs = []
+LAs = []                        #creates array of LA's in the csv
+times = {}                      #creates dictionary with shifts as the key 
+                                # and an array of LA's working at that time as the value
 
 class LA:                       #creates LA Class with a name, hours which should be a list of shift objects
     def __init__(self, name, hours, shifts):
@@ -14,33 +16,48 @@ class LA:                       #creates LA Class with a name, hours which shoul
 
 with open('csv_files\\responses3.csv') as csvfile:                      #might need to change slashes to work on mac
     reader = csv.reader(csvfile, delimiter=',', quotechar='|', skipinitialspace=True)
-    for row in reader:
+    for row in reader:                                                  #iterates through the rows of the csv
         if row_number > 0:
-            print('row number: ' + str(row_number))
+            #print('row number: ' + str(row_number))
             
-            for column in row:
-                if column != '' and column_number > 0:
-                    print('column number: ' + str(column_number))
-                    print(column.replace('"', ''))
+            for column in row:                                          #iterates through the columns of the current row
+                if column != '' and column_number > 0:                  #only looks at columns that are important
+                    # print('column number: ' + str(column_number))
+                    # print(column.replace('"', ''))
+                    column = column.replace('"', '')                    #gets rid of the "" in come of the times
 
-                    if column_number == 1:          #adds the LA's name into LAs
+                    if column_number == 1:                              #adds the LA's name into LAs array
                         LAs.append(LA(column, 0, []))
                     
-                    if column_number == 2:
+                    if column_number == 2:                              #adds the la's hours worked into LAs array
                         LAs[row_number - 1].hours = column
 
-                    if column_number > 2:
+                    if column_number > 2:                               #adds the time to the LA's array of shifts
                         LAs[row_number - 1].shifts.append(column)
+
+                        if not times.__contains__(column):              #if the dictionary of shifts doesnt have this time then 
+                            times[column] = []                          # it gets added with the current LA
+                            times[column].append(LAs[row_number - 1])
+                        else:
+                            times[column].append(LAs[row_number - 1])   #else it just adds the la to that shift
                 
                 column_number += 1
 
         row_number += 1
         column_number = 0
 
-iterator = 0
 
-for ta in LAs:
-    print(iterator)
-    print(ta.name)
-    print(ta.shifts)
-    iterator += 1
+#prints out LA information:
+
+#iterator = 0
+# for ta in LAs:
+#     print(iterator)
+#     print(ta.name)
+#     print(ta.shifts)
+#     iterator += 1
+
+#prints times and the la's in each slot
+for t in times:
+    print(t)
+    for la in times.get(t):
+        print('     '+la.name)
