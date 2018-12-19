464f3bd65cb6cc8f760781655063ee576b8ee35d
James Bury <39829343+jamesbury77@users.noreply.github.com>
2018-12-18 22:00:18 -0500

    scheduler correctly creates LAs from the csv

--- Scheduler.py
+++ Scheduler.py
@@ -1,5 +1,46 @@
 import csv
-with open('csv_files\\responses1.csv') as csvfile:                      #might need to change slashes to work on mac
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
-        print('\n'.join(row))
+
+row_number = 0
+column_number = 0
+
+LAs = []
+
+class LA:                       #creates LA Class with a name, hours which should be a list of shift objects
+    def __init__(self, name, hours, shifts):
+        self.name = name
+        self.hours = hours
+        self.shifts = shifts
+        self.worked = 0
+
+with open('csv_files\\responses3.csv') as csvfile:                      #might need to change slashes to work on mac
+    reader = csv.reader(csvfile, delimiter=',', quotechar='|', skipinitialspace=True)
+    for row in reader:
+        if row_number > 0:
+            print('row number: ' + str(row_number))
+            
+            for column in row:
+                if column != '' and column_number > 0:
+                    print('column number: ' + str(column_number))
+                    print(column.replace('"', ''))
+
+                    if column_number == 1:          #adds the LA's name into LAs
+                        LAs.append(LA(column, 0, []))
+                    
+                    if column_number == 2:
+                        LAs[row_number - 1].hours = column
+
+                    if column_number > 2:
+                        LAs[row_number - 1].shifts.append(column)
+                
+                column_number += 1
+
+        row_number += 1
+        column_number = 0
+
+iterator = 0
+
+for ta in LAs:
+    print(iterator)
+    print(ta.name)
+    print(ta.shifts)
+    iterator += 1
--- app.py
+++ app.py
@@ -8,17 +8,6 @@ app = Flask(__name__)
 def index():
     return 'Index Page'
 
-class LA:                       #creates LA Class with a name, hours which should be a list of shift objects
-    def __init__(self, name, hours):
-        self.name = name
-        self.hours = hours
-        self.worked = 0
-
-class Shift:                    #creates shift class with a time slot which should be a time object and a list of LA's
-    def __init__(self, time_slot, workers):
-        self.workers = workers
-
-
 @app.route('/hello')            #test route
 def hello():
     p1 = LA("James", 10)
