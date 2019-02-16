6b79fd8e6af8e486b98bd6ec37a1a5a51b868d5f
Katherine Griffin <kag728@gmail.com>
2019-02-16 18:13:28 -0500

    updated requirements file

--- lacal.py
+++ lacal.py
@@ -6,7 +6,7 @@ from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+#bootstrap = Bootstrap(app)
 
 @app.route('/')                 #index page
 def index():
