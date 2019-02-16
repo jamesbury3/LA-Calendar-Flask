270063f50975886d15b7613cca5d0033fa718955
Katherine Griffin <kag728@gmail.com>
2019-02-16 18:19:17 -0500

    no bootstrap

--- lacal.py
+++ lacal.py
@@ -4,7 +4,7 @@ import collections
 
 from flask import Flask
 from flask import render_template
-from flask_bootstrap import Bootstrap
+#from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 #bootstrap = Bootstrap(app)
 
